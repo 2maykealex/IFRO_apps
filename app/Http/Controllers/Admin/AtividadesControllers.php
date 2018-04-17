92fb2d76d4f809bb905aaea7de3a27c709762de0
Mayke <mayke.suporte@gmail.com>
2018-04-17 18:09:58 -0400

    atividadesControllers index

--- app/Http/Controllers/Admin/AtividadesControllers.php
+++ app/Http/Controllers/Admin/AtividadesControllers.php
@@ -7,5 +7,7 @@ use App\Http\Controllers\Controller;
 
 class AtividadesControllers extends Controller
 {
-    //
+    public function index(){
+        return view('admin.atividades.index');
+    }
 }
