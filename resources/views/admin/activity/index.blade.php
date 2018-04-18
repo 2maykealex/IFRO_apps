83f3f96482eb06a35e9061fcede817ac42b54f6c
Mayke <mayke.suporte@gmail.com>
2018-04-17 23:11:02 -0400

    criado a view activity

---
+++ resources/views/admin/activity/index.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::page')
+
+@section('content_header')
+    <h1>Lista de atividades complementares</h1>
+    <ol class="breadcrumb">
+        <li><a href=""><Atividades/a></li>
+        <li><a href=""><Lista/a></li>
+    </ol>
+@stop
+
+@section('content')
+    <p>Listando as atividades complementares</p>
+@stop
