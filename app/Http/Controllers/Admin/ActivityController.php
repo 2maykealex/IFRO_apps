c575d95d1903e800635a16a92ed0d8d4a25446b9
Mayke <mayke.suporte@gmail.com>
2018-04-18 15:11:16 -0400

    Definido e estruturado métodos do controller ActivityController

--- app/Http/Controllers/Admin/ActivityController.php
+++ app/Http/Controllers/Admin/ActivityController.php
@@ -4,22 +4,26 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\AtividadesComplementares;
+use App\Models\Activity;
 
 class ActivityController extends Controller
 {
-    public function index(){
-        $activities = AtividadesComplementares::all();
-        //var_dump($activities);
-        return view('admin.activity.index', compact('activities'));
+    public function newActivity(){
+        return view('admin.activity.new');
     }
 
     public function activities(){
-        $activities = AtividadesComplementares::all();
-
-        
-
-        return view('admin.activity.index', compact('activities'));
-
+        $activities = Activity::all();   
+        return view('admin.activity.activities', compact('activities'));
     }
+
+    public function activityStore(Request $request, Activity $activities){
+        
+        $dataForm = $request->all();
+        
+        $activities->activityNew($dataForm);
+        
+    }
+
+
 }
