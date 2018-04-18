1b6685a5564a47153778c8d6027ecb059ec7ae83
Mayke <mayke.suporte@gmail.com>
2018-04-18 02:49:27 -0400

    Implementando view activity\index

--- resources/views/admin/activity/index.blade.php
+++ resources/views/admin/activity/index.blade.php
@@ -3,11 +3,36 @@
 @section('content_header')
     <h1>Lista de atividades complementares</h1>
     <ol class="breadcrumb">
-        <li><a href=""><Atividades/a></li>
-        <li><a href=""><Lista/a></li>
+        <li><a href="{{ route('admin.home') }}">Home</a></li>
+        <li><a href="{{ route('admin.activities') }}">Lista de atividades</a></li>
     </ol>
 @stop
 
 @section('content')
-    <p>Listando as atividades complementares</p>
+    <!-- <h2>Listando as atividades complementares</h2> -->
+
+    <div class="box-body">
+        <table class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Descrição do item</th>
+                    <th>CH Atividade</th>
+                    <th>CH Total item</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($activities as $activity)
+                <tr>
+                    <td>{{ $activity->id }}</td>
+                    <td>{{ $activity->descricao}}</td>
+                    <td>{{ $activity->CHAtividade }}</td>
+                    <td>{{ $activity->CHItem }}</td>
+                </tr>
+                @empty
+                @endforelse
+            </tbody>
+        </table>    
+    </div>
+    
 @stop
