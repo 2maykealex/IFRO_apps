50be144d4ec4df8452704b2207743096d2bb91eb
Mayke <mayke.suporte@gmail.com>
2018-04-18 02:48:45 -0400

    criado metodo Activities

--- app/Http/Controllers/Admin/ActivityController.php
+++ app/Http/Controllers/Admin/ActivityController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\AtividadesComplementares;
 
 class ActivityController extends Controller
 {
     public function index(){
-        return view('admin.activity.index');
+        $activities = AtividadesComplementares::all();
+        //var_dump($activities);
+        return view('admin.activity.index', compact('activities'));
+    }
+
+    public function activities(){
+        $activities = AtividadesComplementares::all();
+
+        
+
+        return view('admin.activity.index', compact('activities'));
+
     }
 }
