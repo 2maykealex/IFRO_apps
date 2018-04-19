bbc53296f89d980153ca406367a29508c6cee4af
Mayke <mayke.suporte@gmail.com>
2018-04-19 18:23:08 -0400

    Criado as Migrations Areas e Courses
    Já realizado o Migrate

---
+++ database/migrations/2018_04_19_175729_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('area_id')->unsigned(); #Eixo tecnológico
+            $table->foreign('area_id')->unsigned()->references('id')->on('areas') ;
+            $table->string('name', 150)->unique();
+            $table->integer('qtSem');
+            $table->integer('chCourse');
+            $table->string('modalidade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
