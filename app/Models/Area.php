81b91724e4eed9310a64078e09faf49dd1546675
Mayke <mayke.suporte@gmail.com>
2018-04-19 18:22:16 -0400

    Criado Models Area e Course

---
+++ app/Models/Area.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Area extends Model
+{   #Area = Eixo tecnológico
+    
+    public $timestamps = false;
+
+    
+}
