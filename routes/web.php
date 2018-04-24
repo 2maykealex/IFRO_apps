e4f8896f74d9ba64be04201c3445a072be7afcf4
Mayke <mayke.suporte@gmail.com>
2018-04-24 17:35:42 -0400

    Rotas area-course e courses-area

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function () {
+
+    $this->get('area-course', 'CourseController@areaCurso')->name('admin.area.course');    
+    $this->get('courses-area', 'AreaController@courses')->name('admin.courses.area');    
+
     $this->post('activity-store', 'ActivityController@activityStore')->name('admin.activity.store');    
     $this->get('activity-new', 'ActivityController@newActivity')->name('admin.activity.new');    
     $this->get('activities', 'ActivityController@activities')->name('admin.activities');    
