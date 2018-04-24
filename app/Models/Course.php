e82b5715c903f7284ac39ea60fe3505e87dfc10e
Mayke <mayke.suporte@gmail.com>
2018-04-24 17:32:52 -0400

    Criada a model Course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Area;
 
 class Course extends Model
-{
+{    
     public $timestamps = false;
+
+    protected $fillable = ['descricao'];
+
+    public function area(){
+        return $this->hasOne(Area::class);
+        
+    }
 }
