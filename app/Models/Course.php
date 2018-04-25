848dd8f52e36f8d94dc851b8ffa0b1156e711716
Mayke <mayke.suporte@gmail.com>
2018-04-25 14:27:12 -0400

    Add metodo courseNew na Model Course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -15,4 +15,26 @@ class Course extends Model
         return $this->belongsTo(Area::class);
         
     }
+
+    public function courseNew($dados):Array {
+
+        $this->area_id     = $dados['area_id'];
+        $this->name        = $dados['name'];
+        $this->qtSem       = $dados['qtSem'];
+        $this->chCourse    = $dados['chCourse'];
+        $this->modalidade  = $dados['modalidade'];
+
+        $course = $this->save();
+
+        if ($course)
+            return [
+                'success' => true,
+                'message' => 'Nova atividade foi cadastrada com sucesso!'
+            ];
+
+        return [
+            'success' => false,
+            'message' => 'Não foi possível realizar este cadastro. Verifique!'
+        ];
+    }
 }
