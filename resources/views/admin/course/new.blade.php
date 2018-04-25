b39c95622e973aaa7fb2691a0a10924e460fde15
Mayke <mayke.suporte@gmail.com>
2018-04-25 14:26:22 -0400

    View new Course

--- resources/views/admin/course/new.blade.php
+++ resources/views/admin/course/new.blade.php
@@ -1,34 +1,47 @@
 @extends('adminlte::page')
 
 @section('content_header')
-    <h1>Cadastrar uma atividade complementar</h1>
+    <h1>Cadastrar um curso</h1>
     <ol class="breadcrumb">
         <li><a href="{{ route('admin.home') }}">Home</a></li>
-        <li><a href="{{ route('admin.activity.new') }}">Nova atividade</a></li>
+        <li><a href="{{ route('admin.activity.new') }}">Novo curso</a></li>
     </ol>
 @stop
 
 @section('content')
     <div class="box">
         <div class="box-header">
-            <h3>Cadastrar nova</h3>
+            <h3>Cadastrar novo</h3>
         </div>
 
         <div class="box-body">
             @include('admin.includes.alerts')
-            <form action="{{ route('admin.activity.store') }}" method="post">
+            <form action="{{ route('admin.course.store') }}" method="post">
                 {!! csrf_field() !!}
+
                 <div class="form-group">
-
-                    <textarea rows="4" cols="50" name="descricao" placeholder="Descrição" class="form-control"></textarea>
-
+                    <select name="area_id" class="form-control">
+                        <option value="">--- Selecione a área do curso ---</option>
+                        @foreach ($areas as $area)
+                            <option value="{{ $area->id }}">{{ $area->descricao }}</option>
+                        @endforeach
+                    </select>
                 </div>
+
                 <div class="form-group">
-                    <input type="text" name="ch_activity" placeholder="CH Atividade" class="form-control">
+                    <input type="text" name="name" placeholder="Nome do curso" class="form-control">
                 </div>
 
                 <div class="form-group">
-                    <input type="text" name="ch_item" placeholder="CH Máxima do item" class="form-control">
+                    <input type="text" name="qtSem" placeholder="Qtd de semestres" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <input type="text" name="chCourse" placeholder="Carga horária" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <input type="text" name="modalidade" placeholder="Modalidade" class="form-control">
                 </div>
 
                 <div class="form-group">
