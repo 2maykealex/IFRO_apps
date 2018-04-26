acbe1c0dfce35a714d91ae8917a81232bb3e72c1
Mayke <mayke.suporte@gmail.com>
2018-04-26 17:22:27 -0400

    Criada a estrutura da Migration People

--- database/migrations/2018_04_26_163144_create_people_table.php
+++ database/migrations/2018_04_26_163144_create_people_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\User;
+use App\Models\City;
 
 class CreatePeopleTable extends Migration
 {
@@ -15,6 +17,27 @@ class CreatePeopleTable extends Migration
     {
         Schema::create('people', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users') ;
+            $table->string('name', 100);
+            
+            $table->string('address', 150);
+            $table->string('number', 10);
+            $table->string('complement', 60);
+            $table->string('neighborhood', 60);
+            $table->string('zipCode', 10);
+
+            $table->integer('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('cities') ;
+
+            $table->string('tel', 15);
+            $table->string('cel', 15);
+
+            
+
+                       
+
+
             $table->timestamps();
         });
     }
