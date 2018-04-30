3b380a1c31031237cf186e74d0b5f8c1a984af80
Mayke <mayke.suporte@gmail.com>
2018-04-30 17:45:03 -0400

    Criada a Model Coordinator
    Metodos Person e People

--- app/Models/Coordinator.php
+++ app/Models/Coordinator.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Coordinator extends Model
 {
-    //
+    public function person(){
+        return $this->morphOne(Person::class, 'personable');
+    }
+
+    public function people(){
+        return $this->morphMany(Person::class, 'personable');
+    }
+
 }
