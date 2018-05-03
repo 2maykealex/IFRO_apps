7c177c653aa22c80121cca02306e3cbccb153ff4
Mayke <mayke.suporte@gmail.com>
2018-05-03 18:03:47 -0400

    Metodo listar Student

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -7,5 +7,12 @@ use App\Http\Controllers\Controller;
 
 class StudentController extends Controller
 {
-    //
+    public function student(){
+        $student = Student::with(['people' , 'course'])->where('id','1')->get();
+
+        echo $student->person->name.'<br>';
+        echo $student->person->address.'<hr>';
+        
+        dd($student);
+    }
 }
