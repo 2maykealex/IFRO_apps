28502ddefe755a2b213e466a7505316c2ac67304
Mayke <mayke.suporte@gmail.com>
2018-05-03 18:11:11 -0400

    Criado Seeder para Students

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CityTableSeeder::class);
         $this->call(PersonTableSeeder::class);
         $this->call(CoordinatorTableSeeder::class);
+        $this->call(StudentTableSeeder::class);
     }
 }
---
+++ database/seeds/StudentTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Student;
+
+class StudentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Student::create([
+            'course_id'    => 2,
+            'person_id'    => 1,
+            'registration' => 88833399922,
+        ]);
+    }
+}
