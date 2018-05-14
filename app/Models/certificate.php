91081447550b59dae0757ac286cc24668fc991f9
Mayke <mayke.suporte@gmail.com>
2018-05-13 20:21:05 -0400

    Criado model e migration Certificates

---
+++ app/Models/certificate.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class certificate extends Model
+{
+    public function person(){
+        return $this->belongsTo(Person::class);
+    }
+
+    public function activity(){
+        return $this->belongsTo(Activity::class);
+    }
+
+    public function certificateNew($data):Array {
+
+        // dd($data);
+        // var_dump($data);
+        $this->person_id             = $data['person_id'];
+        $this->activity_id           = $data['activity_id'];
+        $this->description           = $data['description'];
+        $this->chCertificate         = $data['chCertificate'];
+        $this->chCertificateValided  = $data['chCertificateValided'];
+        $this->image                 = $data['image'];
+
+        $certificate = $this->save();
+
+        if ($certificate)
+            return [
+                'success' => true,
+                'message' => 'Nova atividade foi cadastrada com sucesso!'
+            ];
+
+        return [
+            'success' => false,
+            'message' => 'Não foi possível realizar este cadastro. Verifique!'
+        ];
+    }
+}
---
+++ database/migrations/2018_05_12_084545_create_certificates_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCertificatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('person_id')->unsigned();
+            $table->foreign('person_id')->references('id')->on('people')->onDelete('cascade');
+            $table->integer('activity_id')->unsigned();
+            $table->foreign('activity_id')->references('id')->on('activities')->onDelete('cascade');            
+            $table->string('description', 100)->nullable;
+            $table->integer('chCertificate')->nullable;
+            $table->integer('chCertificateValided')->nullable;
+            $table->string('image', 100)->nullable;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certificates');
+    }
+}
