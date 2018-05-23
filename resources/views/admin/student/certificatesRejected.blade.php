c3a4a5889db94cb870e9361c6394ca7cdc034bf1
Mayke <mayke.suporte@gmail.com>
2018-05-23 14:03:45 -0400

    criada a view certificatesRejected

---
+++ resources/views/admin/student/certificatesRejected.blade.php
@@ -0,0 +1,104 @@
+@extends('adminlte::page')
+
+@section('content_header')
+    <h1>Certificados rejeitados</h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('admin.home') }}">Home</a></li>
+        
+    </ol>
+@stop
+
+@section('content')
+    <div class="box-body">
+        @include('admin.includes.alerts')
+          
+        <table class="table table-bordered table-hover table-responsive">
+            <!-- Sem Tag de cabeçalho -->
+            
+            
+
+            @Foreach ($activities as $key => $activity)
+                
+                <?php $soum = 0; ?>
+
+                <tbody>      
+                    <tr>
+                        <td><h4><strong>{{ $key.' - '.$activity}}</strong></h4></td>       
+                    </tr>                    
+                </tbody>
+                
+                <table class="table table-bordered table-hover table-responsive">
+                    <thead>
+                        <tr>                                
+                            <th></th>
+                            <th>#</th>           
+                            <th>Descrição:</th>      
+                            <th>Link Validação:</th>      
+                            <th>Código:</th>    
+                            <th>CH Válidas:</th>      
+                            <th>Ações:</th>      
+                        </tr>
+                    </thead>
+
+                    <tbody> 
+
+                        <?php
+                            $count = 1;
+                        ?>
+
+                        @forelse ($certificates as $certificate)
+
+                            @if ($certificate->activity_id == $key)
+                                <tr>
+                                    <td></td>
+                                    <td>{{ $count }}</td>
+                                    <td style="width:400px;">{{ $certificate->description}}</td>       
+                                    <td></td>       
+                                    <td></td>       
+                                    
+                                    <td>{{ $certificate->chCertificate}}</td>
+                                    <td>
+                                        
+                                        <a href="{{ url('storage/certificates/'.$certificate->image) }}" target="_blank">
+                                            <button class="btn btn-primary">Imagem</button>
+                                        </a>
+
+                                       
+
+                                    
+                                    </td>
+                                </tr>
+
+                                <?php 
+                                    $count = $count + 1;
+                                    $soum = $soum + $certificate->chCertificate;
+                                    $idActivity = $certificate->activity_id; 
+                                ?>
+                            @endif
+
+
+                        @empty
+                        @endforelse
+
+                        
+
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><strong>TOTAL DE HORAS</strong></td>
+                            <td><strong> <?php echo $soum.' Horas'?> </strong></td>
+                        </tr>
+
+                    </tbody>
+                </table>
+                <br>
+                <hr>
+            @endforeach  
+        </table>  
+    </div>    
+@stop
+
+
+
