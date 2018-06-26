005f1c9b61e36d3bdd93bea398965dd38eb8db83
Mayke <mayke.suporte@gmail.com>
2018-06-26 17:39:16 -0400

    Ajuste na página inicial..

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -1,96 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página inicial</title>
 
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-@extends('adminlte::user')
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
 
-        <title>Página inicial</title>
-@section('content_header')
+        .full-height {
+            height: 100vh;
+        }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-@stop
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-
-
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-
-                <div>
-                    <img src="{{ url('storage/images/if_logo.png') }}" style="max-width:100px;">
-                </div>
-                
-                <div class="title m-b-md">
-                    Instituto Federal de Rondônia
-                </div>
-                <h1><i>Campus</i> Zona Norte - Porto Velho</h1>
-
+</head>
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ route('login') }}">Login</a>
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+                    <a href="{{ route('register') }}">Register</a>
+                @endauth
             </div>
+        @endif
+
+        <div class="content">
+
+            <div>
+                <img src="{{ url('storage/images/if_logo.png') }}" style="max-width:100px;">
+            </div>
+            
+            <div class="title m-b-md">
+                Instituto Federal de Rondônia
+            </div>
+            <h1><i>Campus</i> Zona Norte - Porto Velho</h1>
+
         </div>
-    </body>
+    </div>
+    
+</body>
 </html>
