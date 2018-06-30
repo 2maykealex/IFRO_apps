9ff1c2110f815dbe6cea702e8a66e15e65794705
Mayke <mayke.suporte@gmail.com>
2018-06-30 09:07:00 -0400

    Add método newStudent em Student

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,6 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+    public function newStudent($data):Array{
+
+        $student = new Student;
+        
+        $student->person_id     = $data['person_id'];
+        $student->registration  = $data['registration'];
+        $student->group         = $data['group'];
+        $student->status        = $data['status'];
+        $student->person_id     = $data['person_id'];
+
+        $updated = $student->save();
+
+        // dd($user->id);
+
+        if ($updated){
+            return [
+                $student->id
+            ];
+        }
+    }
+
     public function person(){
         return $this->belongsTo(Person::class);
     }
