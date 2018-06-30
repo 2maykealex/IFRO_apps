25b17aa15640002151180a8fb2a054ddcb8c63dd
Mayke <mayke.suporte@gmail.com>
2018-06-30 09:06:42 -0400

    Add método newUserProfile em UserProfile

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -8,6 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class UserProfile extends Model
 {
     public $timestamps = false;
+
+    public function newUserProfile($data):Array{
+
+        $userProfile = new UserProfile;
+        
+        $userProfile->user_id             = $data['user_id'];
+        $userProfile->profile_access_id   = $data['profile_access_id'];
+
+        $updated = $userProfile->save();
+
+        if ($updated){
+            return [
+                $userProfile->id
+            ];
+        }
+    }
     
     public function profileAccess(){
         return $this->belongsTo(ProfileAccess::class);
