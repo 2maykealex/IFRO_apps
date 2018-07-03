a2b4783be4623c749b19d81411acdf43f8880d1e
Mayke <mayke.suporte@gmail.com>
2018-07-03 17:11:42 -0400

    Add labels nos inputs

--- resources/views/admin/activity/new.blade.php
+++ resources/views/admin/activity/new.blade.php
@@ -11,23 +11,24 @@
 @section('content')
     <div class="box">
         <div class="box-header">
-            <h3>Cadastrar nova</h3>
         </div>
 
         <div class="box-body">
             @include('admin.includes.alerts')
             <form action="{{ route('admin.activity.store') }}" method="post">
                 {!! csrf_field() !!}
+
                 <div class="form-group">
-
+                    <label for="descricao">Descrição da atividade complementar:</label>
                     <textarea rows="4" cols="50" name="descricao" placeholder="Descrição" class="form-control"></textarea>
-
                 </div>
                 <div class="form-group">
+                    <label for="ch_activity">Carga horária máxima por certificado:</label>
                     <input type="text" name="ch_activity" placeholder="CH Atividade" class="form-control">
                 </div>
 
                 <div class="form-group">
+                    <label for="ch_item">Carga horária máxima dessa atividade:</label>
                     <input type="text" name="ch_item" placeholder="CH Máxima do item" class="form-control">
                 </div>
 
