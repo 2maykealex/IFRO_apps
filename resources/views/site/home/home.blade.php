a749063bafd74bd481b8f098d21b65e24bb8fa28
Mayke <mayke.suporte@gmail.com>
2018-07-09 09:48:54 -0400

    iniciado o perfil do aluno

--- resources/views/site/home/home.blade.php
+++ resources/views/site/home/home.blade.php
@@ -1,9 +1,23 @@
 @extends('adminlte::user')
 
+@section('title')
+    Perfil do Aluno
+@stop
+
 @section('content_header')
-    <h1>Dashboard</h1>
+    
+    <br>
+
+    <div class="text-center">
+        <img src="{{ url('storage/profile/1.jpg') }}" class="img-fluid img-circle" alt="Responsive image" height="160" width="150">
+        <h1> MaykeAlexMirandaPlaça </h1>
+        <h4>Curso Superior de Tecnologia em Redes</h4>
+        
+    </div>
 @stop
 
 @section('content')
-    <p>Aluno</p>
+    <div>
+
+    </div>
 @stop
