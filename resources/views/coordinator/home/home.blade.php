4b7c9ee6c6ce0ea7dc878f0147a30a84eb1cab00
Mayke <mayke.suporte@gmail.com>
2018-08-04 09:56:16 -0400

    add Perfil do coordenador

--- resources/views/coordinator/home/home.blade.php
+++ resources/views/coordinator/home/home.blade.php
@@ -17,11 +17,40 @@
 @stop
 
 @section('content')
-    <div>
 
-        <a href="{{ route('change.password','0') }}">
-            <button class="btn btn-warning">Altere sua senha</button>
-        </a>
+    <div class="row">
 
+        <div>
+
+            <a href="{{ route('change.password','0') }}">
+                <button class="btn btn-warning">Altere sua senha</button>
+            </a>
+
+        </div>
+
+        <div class="col-md-6 box" id="formulario">
+
+            <div class="box-body">
+                @include('admin.includes.alerts')
+
+                <form action="{{ route('coordinator.certificate.store') }}" method="post" enctype="multipart/form-data">
+                    {!! csrf_field() !!}
+
+                    <div class="form-group">
+                        <label for="image">Selecione a imagem de sua assinatura digitalizada:</label>
+                        <input type="file" name="image" class="form-control-file" required>
+                    </div>
+
+                    <input id="idCoord" name="idCoord" type="hidden"> 
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-success">Registrar</button>
+                    </div>            
+                </form> 
+                
+            
+            </div>
+        
+        </div>
     </div>
 @stop
