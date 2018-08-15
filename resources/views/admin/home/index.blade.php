ecd7d511305da98dc50fb428277ab1df8f24a038
Mayke <mayke.suporte@gmail.com>
2018-08-15 10:30:09 -0400

    Add Perfil ao Admin

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -1,9 +1,32 @@
 @extends('adminlte::page')
 
+@section('title')
+    Adm
+@stop
+
 @section('content_header')
-    <h1>Dashboard</h1>
+    
+    <br>
+
+    <div class="text-center">
+        <img src="{{ url('storage/profile/1.jpg') }}" class="img-fluid img-circle" alt="Responsive image" height="160" width="150">
+
+        
+    </div>
 @stop
 
 @section('content')
-    <p>You are logged in!</p>
+
+    <div class="row">
+
+        @include('admin.includes.alerts')
+        <div>
+
+            <a href="{{ route('change.password','0') }}">
+                <button class="btn btn-warning">Altere sua senha</button>
+            </a>
+
+        </div>
+        
+    </div>
 @stop
