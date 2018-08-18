fb13898c96d4446a6f54b362bb48694a96a0ccbd
Mayke <mayke.suporte@gmail.com>
2018-08-17 23:15:41 -0400

    Add modal para add assinatura digitalizada para coordinators

--- resources/views/coordinator/home/home.blade.php
+++ resources/views/coordinator/home/home.blade.php
@@ -5,6 +5,79 @@
 @stop
 
 @section('content_header')
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="{{ url('/vendor/jquery/jquery.min.js') }}"></script>
+
+    <style>            
+            /* The Modal (background) */
+        .modal {
+            display: none; /* Hidden by default */
+            position: none; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        }
+
+        /* Modal Content/Box */
+        .modal-content {
+            background-color: #fefefe;
+            margin: 15% auto; /* 15% from the top and centered */
+            padding: 20px;
+            border: 1px solid #888;
+            width: 50%; /* Could be more or less, depending on screen size */
+        }
+
+        /* The Close Button */
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+    </style>
+
+    <script>
+        $(document).ready(function(e) {
+            $("body").delegate("#studentName", "change", function(data){
+
+                //Pegando o valor do select
+                var valor = $(this).val();
+
+                window.location = "/coordinator/certificates/pending/"+valor
+            });
+
+            $("#myModal").hide();
+        }); 
+
+        function showReason(id) {
+            var modal = document.getElementById("myModal");
+            // var inputId = document.getElementById("idCert");
+            // var form = document.getElementById("formCert");
+
+            // inputId.value = id;
+            // alert('ok');
+
+            if (id == -1){
+                modal.style.display = "none";
+            } else{
+                modal.style.display = "block";
+                inputId.focus();
+            }
+        }
+    </script>   
     
     <br>
 
@@ -14,21 +87,62 @@
         <h4>{{ $coordinator->person->course->name }}</h4>
         
     </div>
+
+
 @stop
 
 @section('content')
 
-    <div class="row">
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+        <span class="close" onclick="showReason(-1)">&times;</span>
 
-        <div>
+            <div class="form-group">
+                <div class="form-group">
+                    <h3>Adicionar assinatura digitalizada</h3>                
+                </div>
 
-            <a href="{{ route('change.password','0') }}">
-                <button class="btn btn-warning">Altere sua senha</button>
-            </a>
+                <form action="{{ route('coordinator.sign.store') }}" method="post" enctype="multipart/form-data">
+                    {!! csrf_field() !!}
 
+                    <div class="form-group">
+                        <label for="image">Selecione a imagem de sua assinatura digitalizada:</label>
+                        <input type="file" name="image" class="form-control-file" required>
+                    </div>
+
+                    <input id="idCoord" name="idCoord" type="hidden" value="{{ $coordinator->id }}"> 
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-success">Registrar</button>
+                    </div>            
+                </form>  
+
+                @isset($coordinator->signature)
+                    <div class="form-group">
+                        <img src="{{ url('storage/signatures/'.$coordinator->signature) }}" class="img-fluid " alt="Responsive image">
+                    </div>        
+                @endisset
+                             
+                    
+            </div>
         </div>
+    </div> 
 
-        <div class="col-md-6 box" id="formulario">
+    <div class="text-center"    >
+
+        <a href="{{ route('change.password','0') }}">
+            <button class="btn btn-warning">Altere sua senha</button>
+        </a>
+
+        <button class="btn btn-primary" onclick="showReason(1)">Adicionar sua assinatura digitalizada</button>
+
+    </div>
+
+    <!-- <div class="row"> 
+
+        <div class="col-md-2 box" id="formulario">
 
             <div class="box-body">
                 @include('admin.includes.alerts')
@@ -52,9 +166,8 @@
 
         @isset($coordinator->signature)
             <div class="form-group">
-            <img src="{{ url('storage/signatures/'.$coordinator->signature) }}" class="img-fluid " alt="Responsive image">
+                <img src="{{ url('storage/signatures/'.$coordinator->signature) }}" class="img-fluid " alt="Responsive image">
             </div>        
-
         @endisset
-    </div>
+    </div> -->
 @stop
