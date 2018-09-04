0d05d956b472042374b760f398a9897a974aba92
Mayke <mayke.suporte@gmail.com>
2018-09-04 09:12:25 -0400

    DomPDF

--- config/app.php
+++ config/app.php
@@ -145,7 +145,8 @@ return [
         Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,        
+        Illuminate\View\ViewServiceProvider::class,  
+        Barryvdh\DomPDF\ServiceProvider::class, 
 
         /*
          * Package Service Providers...
@@ -208,6 +209,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
