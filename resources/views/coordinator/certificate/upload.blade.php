ffa170148a8a063936d2dcba7025f82e10af1d47
Mayke <mayke.suporte@gmail.com>
2018-09-06 00:31:28 -0400

    Varias alterações; ajustes nas rotar dos formulários; adequação para editar certificado

--- resources/views/coordinator/certificate/upload.blade.php
+++ resources/views/coordinator/certificate/upload.blade.php
@@ -13,7 +13,12 @@
     <script>
         $(document).ready(function(e) {
 
-            $("#formulario").hide();             // carrega a página com o formulário de envio oculto
+            @if (!isset($id))
+                $("#formulario").hide();  // carrega a página com o formulário de envio oculto
+                showImage();
+            @else
+                $("#imgCert").hide();  // carrega a página com o formulário de envio oculto
+            @endif
 
             $("body").delegate("#studentName", "change", function(data){
                 //Pegando o valor do select
@@ -27,32 +32,48 @@
                 } else {
                     $("#formulario").show();
                 };
-            });
+            });            
         });
+
+        function showImage(){
+            var htmlText;
+            htmlText = "  <div class='form-group'>";
+            htmlText =  htmlText + "<label for='image'>Selecione a imagem do certificado no seu computador:</label>";
+            htmlText =  htmlText + "<input type='file' name='image' class='form-control-file' required > </div>";
+
+            document.getElementById('imgCert').innerHTML = htmlText;
+        }
+
+        function btnClickedShowImage(){
+            showImage();
+            $("#imgCert").show();
+        }
     </script>   
 
 @stop
 
 @section('content')
 
-    <div class="row">
-        <div class="col-md-12">        
-            <div class="form-group">
+    @if (!isset($id))
+        <div class="row">
+            <div class="col-md-12">        
+                <div class="form-group">
 
-                <label for="studentName">
-                    Incluir certificado para o Aluno:
-                </label>
-                
-                <select name="studentName" class="form-control" id="studentName" >
-                    <option value="">Selecionar o aluno:</option>
+                    <label for="studentName">
+                        Incluir certificado para o Aluno:
+                    </label>
+                    
+                    <select name="studentName" class="form-control" id="studentName" >
+                        <option value="">Selecionar o aluno:</option>
 
-                    @foreach ($students as $student)                         
-                        <option value="{{ $student->person->id }}"> {{ $student->person->name }}</option>                        
-                    @endforeach  
-                </select>
-            </div>
-        </div> 
-    </div>
+                        @foreach ($students as $student)                         
+                            <option value="{{ $student->person->id }}"> {{ $student->person->name }}</option>                        
+                        @endforeach  
+                    </select>
+                </div>
+            </div> 
+        </div>
+    @endif
 
     <div class="box" id="formulario">
         <div class="box-header">
@@ -62,7 +83,12 @@
         <div class="box-body">
             @include('admin.includes.alerts')
 
-            <form action="{{ route('coordinator.certificate.store') }}" method="post" enctype="multipart/form-data">
+            @isset($certificate)
+                <form action="{{ route('coordinator.certificate.update') }}" method="post" enctype="multipart/form-data">
+            @else
+                <form action="{{ route('coordinator.certificate.store') }}" method="post" enctype="multipart/form-data">
+            @endif
+
                 {!! csrf_field() !!}
 
                 <div class="form-group">
@@ -70,57 +96,78 @@
                     <select name="activity_id" class="form-control" required>
                         <option value="">--- Selecione a atividade ---</option>
                         @foreach ($activities as $activity)
-                            <option value="{{ $activity->id }}">{{ $activity->id }} - {{ $activity->descricao }}</option>
+                            <option value="{{ $activity->id }}" <?php if (isset($certificate)) {if ($activity->id == $certificate->activity_id) {echo "Selected";}} ?>>{{ $activity->id }} - {{ $activity->descricao }}</option>
                         @endforeach
                     </select>
                 </div>
 
                 <div class="form-group">
                     <label for="description">Descrição do certificado</label>
-                    <input type="text" name="description" placeholder="Ex.: Curso de lógica" class="form-control" required>
+                    <input type="text" name="description" placeholder="Ex.: Curso de lógica" class="form-control" required <?php if (isset($certificate->description)) {echo "value = '$certificate->description'";} ?>>
                 </div>
                 
                 <div class="form-group">
                     <label for="local">O certificado foi emitido em:</label>
-                    <input type="text" name="local" placeholder="Local, Cidade e Estado onde foi emitido o certificado, mesmo que o curso foi à distância. Ex. Porto Velho - RO" class="form-control" required>
+                    <input type="text" name="local" placeholder="Local, Cidade e Estado onde foi emitido o certificado, mesmo que o curso foi à distância. Ex. Porto Velho - RO" class="form-control" required <?php if (isset($certificate->local)) {echo "value = '$certificate->local'";} ?>>
                 </div>
 
                 <div class="form-group">
                     <label for="period">Período que o curso estava envolvido:</label>
-                    <input type="text" name="period" placeholder="Ex: Janeiro/2018;  Maio/2018 a Junho/2018" class="form-control" required>
+                    <input type="text" name="period" placeholder="Ex: Janeiro/2018;  Maio/2018 a Junho/2018" class="form-control" required <?php if (isset($certificate->period)) {echo "value = '$certificate->period'";} ?>>
                 </div>
 
-                <div class="form-group">
-                    <label for="image">Selecione a imagem do certificado no seu computador:</label>
-                    <input type="file" name="image" class="form-control-file" required>
+                <div class="form-inline">
+                
+                    <div id="imgCert" class="form-group">
+
+                    </div>
+
+                    @isset($certificate)
+                        <input id="update" name="update" type="hidden" value="1"> 
+                        <input id="certId" name="certId" type="hidden" value="{{ $certificate->id }}"  > 
+
+                        <div class="form-group">
+                            <button type="button" id="btnImgCert" onclick="btnClickedShowImage();" class="btn btn-success">Alterar a imagem do Certificado?</button>
+                        </div>
+
+                        <div class="form-group">
+                            <a href="{{ url('storage/certificates/'.$certificate->image) }}" target="_blank">
+                                <img src="{{ url('storage/certificates/'.$certificate->image) }}" class="img-fluid" alt="Responsive image" height="180" width="210"> 
+                            </a>
+                        </div>                    
+                    @endif
                 </div>
+                <br>
 
                 <div class="form-group">
                     <label for="chCertificate">Carga horária registrada no certificado:</label>
-                    <input type="number" name="chCertificate" placeholder="Ex.: 80" class="form-control" required>
+                    <input type="number" name="chCertificate" placeholder="Ex.: 80" class="form-control" required <?php if (isset($certificate->chCertificate)) {echo "value = $certificate->chCertificate";} ?>>
                 </div>
 
                 <input id="prodId" name="certificateValided" type="hidden" value="0"> <!-- 0 não validado -->
                 
-                <input id="idStudent" name="idStudent" type="hidden"> 
+                <input id="idStudent" name="idStudent" type="hidden" <?php if (isset($id)) {echo "value='$students->id'";} ?>> 
 
                 <div class="form-group">
                     <label for="linkValidation">Se houver um link de validação on-line, coloque aqui:</label>
-                    <input type="text" name="linkValidation" placeholder="Ex.: https://academy.especializati.com.br/verificar-certificado" class="form-control">
+                    <input type="text" name="linkValidation" placeholder="Ex.: https://academy.especializati.com.br/verificar-certificado" class="form-control" <?php if (isset($certificate->linkValidation)) {echo "value = $certificate->linkValidation";} ?>>
                 </div>
 
                 <div class="form-group">
                     <label for="validationCode">Se houver código de validação para o certificado, coloque aqui:</label>
-                    <input type="text" name="validationCode" placeholder="Ex.: 2tSTCXBFyq" class="form-control">
+                    <input type="text" name="validationCode" placeholder="Ex.: 2tSTCXBFyq" class="form-control" <?php if (isset($certificate->validationCode)) {echo "value = $certificate->validationCode";} ?>>
                 </div>
 
-                <div class="form-group">
-                    <button type="submit" class="btn btn-success">Registrar</button>
-                </div>            
-            </form> 
-            
-        
-        </div>
-    
+                @if (!isset($id))
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-success">Salvar novo</button>
+                    </div>      
+                @else
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-success">Atualizar</button>
+                    </div>                
+                @endif      
+            </form>         
+        </div>    
     </div>
 @stop
--- resources/views/site/certificate/upload.blade.php
+++ resources/views/site/certificate/upload.blade.php
@@ -5,6 +5,50 @@
     <ol class="breadcrumb">
         
     </ol>
+
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+
+    <script src="{{ url('/vendor/jquery/jquery.min.js') }}"></script>
+
+    <script>
+        $(document).ready(function(e) {
+
+            @if (!isset($id))
+                $("#formulario").hide();  // carrega a página com o formulário de envio oculto
+                showImage();
+            @else
+                $("#imgCert").hide();  // carrega a página com o formulário de envio oculto
+            @endif
+
+            $("body").delegate("#studentName", "change", function(data){
+                //Pegando o valor do select
+                
+                var valor = $(this).val();
+                
+                $("#idStudent").val(valor);
+                
+                if ($("#studentName").val() == '') {
+                    $("#formulario").hide();
+                } else {
+                    $("#formulario").show();
+                };
+            });            
+        });
+
+        function showImage(){
+            var htmlText;
+            htmlText = "  <div class='form-group'>";
+            htmlText =  htmlText + "<label for='image'>Selecione a imagem do certificado no seu computador:</label>";
+            htmlText =  htmlText + "<input type='file' name='image' class='form-control-file' required > </div>";
+
+            document.getElementById('imgCert').innerHTML = htmlText;
+        }
+
+        function btnClickedShowImage(){
+            showImage();
+            $("#imgCert").show();
+        }
+    </script>
 @stop
 
 @section('content')
@@ -14,7 +58,13 @@
 
         <div class="box-body">
             @include('admin.includes.alerts')
-            <form action="{{ route('site.certificate.store') }}" method="post" enctype="multipart/form-data">
+            
+            @isset($certificate)
+                <form action="{{ route('site.certificate.update') }}" method="post" enctype="multipart/form-data">
+            @else
+                <form action="{{ route('site.certificate.store') }}" method="post" enctype="multipart/form-data">
+            @endif
+
                 {!! csrf_field() !!}
 
                 <div class="form-group">
@@ -22,51 +72,75 @@
                     <select name="activity_id" class="form-control" required>
                         <option value="">--- Selecione a atividade ---</option>
                         @foreach ($activities as $activity)
-                            <option value="{{ $activity->id }}">{{ $activity->id }} - {{ $activity->descricao }}</option>
+                            <option value="{{ $activity->id }}" <?php if (isset($certificate)) {if ($activity->id == $certificate->activity_id) {echo "Selected";}} ?>>{{ $activity->id }} - {{ $activity->descricao }}</option>
                         @endforeach
                     </select>
                 </div>
 
                 <div class="form-group">
                     <label for="description">Descrição do certificado</label>
-                    <input type="text" name="description" placeholder="Ex.: Curso de lógica" class="form-control" required>
+                    <input type="text" name="description" placeholder="Ex.: Curso de lógica" class="form-control" required <?php if (isset($certificate->description)) {echo "value = '$certificate->description'";} ?>>
                 </div>
                 
                 <div class="form-group">
                     <label for="local">O certificado foi emitido em:</label>
-                    <input type="text" name="local" placeholder="Local, Cidade e Estado onde foi emitido o certificado, mesmo que o curso foi à distância. Ex. Porto Velho - RO" class="form-control" required>
+                    <input type="text" name="local" placeholder="Local, Cidade e Estado onde foi emitido o certificado, mesmo que o curso foi à distância. Ex. Porto Velho - RO" class="form-control" required <?php if (isset($certificate->local)) {echo "value = '$certificate->local'";} ?>>
                 </div>
 
                 <div class="form-group">
                     <label for="period">Período que o curso estava envolvido:</label>
-                    <input type="text" name="period" placeholder="Ex: Janeiro/2018;  Maio/2018 a Junho/2018" class="form-control" required>
+                    <input type="text" name="period" placeholder="Ex: Janeiro/2018;  Maio/2018 a Junho/2018" class="form-control" required <?php if (isset($certificate->period)) {echo "value = '$certificate->period'";} ?>>
                 </div>
 
-                <div class="form-group">
-                    <label for="image">Selecione a imagem do certificado no seu computador:</label>
-                    <input type="file" name="image" class="form-control-file" required>
+                <div class="form-inline">
+                    
+                    <div id="imgCert" class="form-group">
+
+                    </div>
+
+                    @isset($certificate)
+                        <input id="update" name="update" type="hidden" value="1"> 
+                        <input id="certId" name="certId" type="hidden" value="{{ $certificate->id }}"  > 
+
+                        <div class="form-group">
+                            <button type="button" id="btnImgCert" onclick="btnClickedShowImage();" class="btn btn-success">Alterar a imagem do Certificado?</button>
+                        </div>
+                        
+                        <div class="form-group">
+                            <a href="{{ url('storage/certificates/'.$certificate->image) }}" target="_blank">
+                                <img src="{{ url('storage/certificates/'.$certificate->image) }}" class="img-fluid" alt="Responsive image" height="180" width="210"> 
+                            </a>
+                        </div>                    
+                    @endif
                 </div>
+                <br>
 
                 <div class="form-group">
                     <label for="chCertificate">Carga horária registrada no certificado:</label>
-                    <input type="number" name="chCertificate" placeholder="Ex.: 80" class="form-control" required>
+                    <input type="number" name="chCertificate" placeholder="Ex.: 80" class="form-control" required <?php if (isset($certificate->chCertificate)) {echo "value = $certificate->chCertificate";} ?>>
                 </div>
 
                 <input id="prodId" name="certificateValided" type="hidden" value="0"> <!-- 0 não validado -->
 
                 <div class="form-group">
                     <label for="linkValidation">Se houver um link de validação on-line, coloque aqui:</label>
-                    <input type="text" name="linkValidation" placeholder="Ex.: https://academy.especializati.com.br/verificar-certificado" class="form-control">
+                    <input type="text" name="linkValidation" placeholder="Ex.: https://academy.especializati.com.br/verificar-certificado" class="form-control" <?php if (isset($certificate->linkValidation)) {echo "value = $certificate->linkValidation";} ?>>
                 </div>
 
                 <div class="form-group">
                     <label for="validationCode">Se houver código de validação para o certificado, coloque aqui:</label>
-                    <input type="text" name="validationCode" placeholder="Ex.: 2tSTCXBFyq" class="form-control">
+                    <input type="text" name="validationCode" placeholder="Ex.: 2tSTCXBFyq" class="form-control" <?php if (isset($certificate->validationCode)) {echo "value = $certificate->validationCode";} ?>>
                 </div>
 
-                <div class="form-group">
-                    <button type="submit" class="btn btn-success">Registrar</button>
-                </div>            
+                @if (!isset($id))
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-success">Salvar novo</button>
+                    </div>      
+                @else
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-success">Atualizar</button>
+                    </div>                
+                @endif             
             </form>        
         </div>    
     </div>
